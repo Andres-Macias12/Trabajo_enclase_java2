b5e27d06ad1b64e88bb52f612947a68e99ef3f18
Andres Macias <andresmacias978@gmail.com>
2024-07-18 23:40:59 -0500

    Trabajo Java2

---
+++ src/main/java/co/com/ps/c24a/controller/AddressController.java
@@ -0,0 +1,49 @@
+package co.com.ps.c24a.controller;
+
+import co.com.ps.c24a.entity.Address;
+import co.com.ps.c24a.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/addresses")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable Long id) {
+        Optional<Address> address = addressService.getAddressById(id);
+        return address.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Address>> getAllAddresses() {
+        List<Address> addresses = addressService.getAddressAll();
+        return ResponseEntity.ok(addresses);
+    }
+
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody Address address) {
+        Address savedAddress = addressService.saveAddress(address);
+        return ResponseEntity.ok(savedAddress);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddress(@PathVariable Long id, @RequestBody Address address) {
+        Address updatedAddress = addressService.updateAddress(id, address);
+        return ResponseEntity.ok(updatedAddress);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAddress(@PathVariable Long id) {
+        addressService.deleteAddress(id);
+        return ResponseEntity.noContent().build();
+    }
+}
