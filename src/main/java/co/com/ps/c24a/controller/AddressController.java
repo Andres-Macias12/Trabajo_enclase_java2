aabecc09f06b9c259220c80da8d7f28a1fa140d9
Andres Macias <andresmacias978@gmail.com>
2024-07-20 11:20:44 -0500

    trabajo java2

--- src/main/java/co/com/ps/c24a/controller/AddressController.java
+++ src/main/java/co/com/ps/c24a/controller/AddressController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/addresses")
+@RequestMapping("/address")
 public class AddressController {
 
     @Autowired
